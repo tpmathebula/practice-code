d8b093753390c32f55911bd68b056e7a56a8f799
Themba Mathebula <tpmathebula@gmail.com>
2021-02-13 09:44:03 +0200

    initial commit

---
+++ Newnum.java
@@ -0,0 +1,19 @@
+public class Newnum 
+{
+	
+	
+	public static void main(String[] args)
+	{
+		int[] numbers = new int[5];
+
+		numbers[0] = 4;
+		
+		System.out.println(numbers[0]);
+		
+		
+		
+		
+		
+	}
+	
+}
