fa19c18e9e3988ca17ebfab7c597aba1017521ea
Themba Mathebula <tpmathebula@gmail.com>
2021-02-13 10:17:54 +0200

    added sample code

---
+++ datastructure.java
@@ -0,0 +1,9 @@
+public class Datastructure {
+    
+    public static void main(String[] args)
+    {
+        boolean var = true;
+
+        System.out.println("!yes : " + (var ^ var));
+    }
+}       
